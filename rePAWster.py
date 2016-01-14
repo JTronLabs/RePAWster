d84e02c02f648c61625dee0e96a39cbc6cb42874
jtronlabs <jtronlabs@gmail.com>
2016-01-14 15:01:25 -0500

    changed criteria of dog pictures to submit, to make it more leniant

--- rePAWster.py
+++ rePAWster.py
@@ -3,20 +3,20 @@ import random
 import time
 from datetime import datetime, timedelta
 
-dog_subreddits = ["dogpictures",
-    "DogsOnHardwoodFloors",
-    "DogsWithEyebrows",
-    "DogsWearingHats",
-    "lookatmydog",
-    "dogswearingglasses",
-    "woof_irl",
-    "BelgianMalinois",
-    "bergerbelge",
-    "germanshepard",
-    "labrador",
-    "beagle",
-    "DobermanPinscher",
-    "CorgiGifs"]
+dog_subreddits = {"dogpictures":100,
+    "DogsOnHardwoodFloors":20,
+    "DogsWithEyebrows":50,
+    "DogsWearingHats":50,
+    "lookatmydog":75,
+    "dogswearingglasses":7,
+    "woof_irl":80,
+    "BelgianMalinois":20,
+    "bergerbelge":25,
+    "germanshepard":15,
+    "labrador":50,
+    "beagle":40,
+    "DobermanPinscher":30,
+    "CorgiGifs":100}
 
 user_agent = "/r/top_dogs submission gathering (by /u/rePAWster)"
 num_submissions_to_grab_from_each_dog_subreddit = 25 #use a max of 25 please
@@ -25,13 +25,11 @@ def check_subreddit_for_submissions(subreddit_name,reddit_obj):
     print("/r/"+subreddit_name+" is being checked for submissions...")
     subreddit_submissions = subreddit_posts_to_list(reddit_obj,subreddit_name)
 
-    median_karma,min_karma,max_karma = karma_stats(subreddit_submissions)
-
     approved_submissions = []
 
     for submission in subreddit_submissions:
         karma = submission.ups - submission.downs
-        if(post_is_new_enough(submission.created) and post_is_good_enough(median_karma,max_karma,min_karma,karma) and is_image_url(submission.url) ):
+        if(post_is_new_enough(submission.created) and post_is_good_enough(subreddit_name,karma) and is_image_url(submission.url) ):
             approved_submissions.append(submission)
 
     return approved_submissions
@@ -41,14 +39,30 @@ def post_is_new_enough(creation_time):
     cutoff = datetime.now() - timedelta(days=7)
     return submission_creation_datetime > cutoff
 
+def post_is_good_enough(subreddit_name,post_karma):
+    return post_karma > dog_subreddits[subreddit_name]
+"""
 def post_is_good_enough(median_karma,max_karma,min_karma,post_karma):
     return post_karma > median_karma and post_karma > max_karma * 0.7
 
+def karma_stats(submissions):
+    karma_values = []
+
+    for submission in submissions:
+        submission_karma = submission.ups - submission.downs
+        karma_values.append(submission_karma)
+
+    karma_values.sort()
+
+    #returns median, min, and max karma values
+    return karma_values[int(len(karma_values)/2)], karma_values[0], karma_values[len(karma_values)-1]
+
+"""
 def is_image_url(url):
     return True
 
 def submit_to_my_subreddit(reddit_obj,submissions):
-    print("Submitting links to /r/top_dogs subreddit")
+    print("Submitting "+str(len(submissions))+" links to /r/top_dogs subreddit")
     reddit_obj.login("rePAWster","Hundr3d",disable_warning=True)
 
     while(len(submissions)>0 ):
@@ -80,18 +94,6 @@ def subreddit_posts_to_list(reddit_obj,subreddit_name):
     for submission in subreddit_submissions:
         submissions.append(submission)
     return submissions
-
-def karma_stats(submissions):
-    karma_values = []
-
-    for submission in submissions:
-        submission_karma = submission.ups - submission.downs
-        karma_values.append(submission_karma)
-
-    karma_values.sort()
-
-    #returns median, min, and max karma values
-    return karma_values[int(len(karma_values)/2)], karma_values[0], karma_values[len(karma_values)-1]
 
 def find_and_submit_posts():
     r = praw.Reddit(user_agent=user_agent)
